80af40638068e59f1145de8b7df851c38f2c947a
Murdock135 <particlegriffin@gmail.com>
2024-12-23 16:39:20 -0600

    Learned how to query an sql db pythonically

--- 12_querying_csv.py
+++ 12_querying_csv.py
@@ -1,23 +1,34 @@
 import pandas as pd
 from sqlalchemy import create_engine, inspect
 from dotenv import load_dotenv, find_dotenv
-import os
+from langchain_community.utilities import SQLDatabase
 
+# Step 0: load environment variables
 _ = load_dotenv(find_dotenv())
 
-# Load dataset as pandas dataframe
+# Step 1: Load dataset as pandas dataframe
 data_path = 'titanic.csv'
 df = pd.read_csv(data_path)
 
-# Create SQL engine
-engine = create_engine("sqlite:///titanic.db")
+# Step 2: Convert Dataframe into SQL table
+# Step 2.1: Create SQL engine
+db_name = 'titanic'
+engine = create_engine(f"sqlite:///{db_name}.db")
 
-# Load csv as sql table
+# Step 2.2: Write csv into sql table
 inspector = inspect(engine)
-if "titanic" in inspector.get_table_names():
-    print("Table 'titanic' already exists. Data not written to database.")
+if db_name in inspector.get_table_names():
+    print(f"Table {db_name} already exists. Data not written to database.")
 else:
     # Write to SQL database only if table doesn't exist
-    df.to_sql("titanic", engine, index=False)
+    df.to_sql(db_name, engine, index=False)
     print("Data written to the database.")
+
+# Step 3: Load database
+db = SQLDatabase(engine)
+print(db.dialect)
+print(db.get_usable_table_names())
+
+# Step 4: Query the data
+print(db.run("SELECT * FROM titanic WHERE Age <2;"))
 
